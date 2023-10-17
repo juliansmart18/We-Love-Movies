9954631ec0a903a4a45e0da5bb0db78214a2786a
Julian <juliansmart1996@gmail.com>
2023-10-17 15:22:37 -0400

    all tests passing

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ const app = express();
 const cors = require("cors");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
 
 // Not found handler
 app.use((req, res, next) => {
